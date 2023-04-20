fd3495158fefb16f38ef7e442391a1cdecca9c64
powler <1119272440@qq.com>
2023-04-20 22:07:08 +0800

    finish ch5 f, make functions to print in kernel

---
+++ ch5/f/kernel.asm
@@ -0,0 +1,31 @@
+; nasm -f elf kernel.asm -o kernel.o
+; ld -s kernel.o -o kernel
+
+SELECTOR_KERNEL_CS equ 8
+
+extern cstart
+
+extern gdt_ptr
+
+[SECTION .bss]
+StackSpace  resb 2*1024
+StackTop:
+
+[section .text]
+
+global _start
+
+_start:
+    mov esp, StackTop
+
+    sgdt [gdt_ptr]
+    call cstart
+    lgdt [gdt_ptr]
+
+    jmp SELECTOR_KERNEL_CS:csinit
+
+csinit:
+    push 0
+    popfd
+
+    hlt
