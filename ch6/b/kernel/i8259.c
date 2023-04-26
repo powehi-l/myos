e942a2abec3f1b0c1cdd6591579d82c3f8cd7b50
powler <1119272440@qq.com>
2023-04-26 21:32:35 +0800

    ch6 b finish, restore the registers when interrupt happen, and switch the stack from user to kernel.

---
+++ ch6/b/kernel/i8259.c
@@ -0,0 +1,37 @@
+#include "type.h"
+#include "const.h"
+#include "protect.h"
+#include "proto.h"
+
+//initial 8259A
+//==========================
+
+PUBLIC void init_8259A()
+{
+    out_byte(INT_M_CTL, 0x11);
+
+    out_byte(INT_S_CTL, 0x11);
+
+    out_byte(INT_M_CTLMASK, INT_VECTOR_IRQ0);
+
+    out_byte(INT_S_CTLMASK, INT_VECTOR_IRQ8);
+
+    out_byte(INT_M_CTLMASK, 0x4);
+
+    out_byte(INT_S_CTLMASK, 0x2);
+
+    out_byte(INT_M_CTLMASK, 0x1);
+
+    out_byte(INT_S_CTLMASK, 0x1);
+
+    out_byte(INT_M_CTLMASK, 0xFE);
+
+    out_byte(INT_S_CTLMASK, 0xFF);
+}
+
+PUBLIC void spurious_irq(int irq)
+{
+        disp_str("spurious_irq: ");
+        disp_int(irq);
+        disp_str("\n");
+}
