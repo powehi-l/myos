33c1160178118db62d3459a03b6447f90df05a47
powler <1119272440@qq.com>
2023-05-23 10:28:03 +0800

    finish ch6 g, make make interrupt reenter success

---
+++ ch6/g/include/proto.h
@@ -0,0 +1,24 @@
+/* klib.asm */
+
+PUBLIC void out_byte(u16 port, u8 value);
+PUBLIC u8 in_byte(u16 port);
+PUBLIC void disp_str(char* info);
+PUBLIC void disp_color_str(char* info, int color);
+
+//protect.c
+PUBLIC void init_prot();
+PUBLIC void init_8259A();
+
+//klib.c
+PUBLIC void delay(int time);
+
+//kernel.asm
+void restart();
+
+//main.c
+void TestA();
+void TestB();
+void TestC();
+
+PUBLIC void disp_int(int input);
+
