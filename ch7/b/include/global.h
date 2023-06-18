67e3ca1d84e224b1e6957e3bcb96f5d3e4f61619
powler <1119272440@qq.com>
2023-06-18 15:50:52 +0800

    ch7 b finish, read the scan code and break code for a key from 8042 and print to screen

---
+++ ch7/b/include/global.h
@@ -0,0 +1,24 @@
+#ifdef GLOBAL_VARIABLES_HERE
+#undef EXTERN
+#define EXTERN
+#endif
+
+EXTERN int disp_pos;
+
+EXTERN u8 gdt_ptr[6];
+EXTERN DESCRIPTOR gdt[GDT_SIZE];
+
+EXTERN u8 idt_ptr[6];
+EXTERN DESCRIPTOR idt[IDT_SIZE];
+
+EXTERN u32 k_reenter;
+
+EXTERN TSS tss;
+EXTERN PROCESS* p_proc_ready;
+
+extern PROCESS proc_table[];
+extern char task_stack[];
+extern TASK task_table[];
+extern irq_handler irq_table[];
+
+EXTERN int ticks;
