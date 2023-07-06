a74c07a6ca37cb53f516047048d8fbf923cc04bd
powler <1119272440@qq.com>
2023-07-06 12:01:43 +0800

    ch7 l finish, complete enter and backspace

---
+++ ch7/l/include/console.h
@@ -0,0 +1,20 @@
+#ifndef _CONSOLE_H_
+#define _CONSOLE_H_
+
+typedef struct s_console
+{
+  unsigned int current_start_addr;  //place to be display
+  unsigned int original_addr;       //current console's display memory position
+  unsigned int v_mem_limit;         //size if current display memory
+  unsigned int cursor;              //cursor in current console
+}CONSOLE;
+
+#define SCR_UP 1
+#define SCR_DN -1
+
+#define SCREEN_SIZE (80 * 25)
+#define SCREEN_WIDTH 80
+
+#define DEFAULT_CHAR_COLOR 0x07
+
+#endif
